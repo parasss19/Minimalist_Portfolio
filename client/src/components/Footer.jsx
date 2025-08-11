ec06b9febca8fdf361d9b99f1bd832e58fad39cd
Paras <mehtaparas1901@gmail.com>
2025-08-11 12:43:43 +0530

    peerlist link added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <div className='my_container bg-[#212121] border border-[#353434] mb-3 rounded-xl'>
       
-      <div className="mx-2 mt-4 rounded-xl flex justify-between items-center">
+      <div className="mx-2 mt-4 rounded-xl flex flex-col gap-4 sm:flex-row justify-between items-center">
         <div className="mx-3">
           <DotTitle title="Follow Me"/>
         </div>
@@ -59,6 +59,21 @@ const Footer = () => {
             }
           >
             <FaLinkedinIn className="w-[18px] h-[18px] sm:w-6 sm:h-6"/>
+          </a>
+          <a 
+            href="https://peerlist.io/paras19" 
+            target='blank'
+            className="bg-black p-1.5 rounded-full hover:-translate-y-1 transition-transform ease-linear"
+            onClick={() =>
+              sendEvent({
+                action: "social_click",
+                category: "Social",
+                label: "Linkedin",
+                value: 1,
+              })
+            }
+          >
+            <img src="/Peerlist.png" alt="peerlist icon" className="w-[22px] h-[22px] sm:w-[30px] sm:h-[30px]"/>
           </a>
         </div>
       </div>
